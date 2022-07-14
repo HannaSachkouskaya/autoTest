8f6da0bf252729bc36ea7be8e8acd1563950c08a
Hanna Sachkouskaya <Hanna_Sachkouskaya@com>
2022-07-14 14:58:43 +0300

    add new file

--- myTest/test/specs/test.yahoo.js
+++ myTest/test/specs/test.yahoo.js
@@ -8,26 +8,24 @@ const sentFolderButton = require ('../pageobjects/sent.Folder');
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
-        await browser.url("https://mail.yahoo.com/");
-
-       await textEmail.signInButton.isDisplayed();
+       await browser.url("https://mail.yahoo.com/");
+       await expect (await textEmail.signInButton.waitForDisplayed()).is.equal(true);
        await textEmail.signInButton.click();
        await textEmail.login('anntest547@yahoo.com');
-       await textPassword.nextButton.waitForDisplayed();
+       await expect (await textPassword.nextButton.waitForDisplayed()).is.equal(true);
        await textPassword.nextButton.click();
        await textPassword.loginPassword('Leto354Pas');
-       await textPassword.nextButtonPassword.waitForDisplayed();
+       await expect (await textPassword.nextButtonPassword.waitForDisplayed()).is.equal(true);
        await textPassword.nextButtonPassword.click();
-       await textPassword.accountName.waitForDisplayed();
-       await textPassword.accountName.isEqual(true);
+       await expect (await textPassword.accountName.waitForDisplayed()).is.equal(true);
         });
 
     it('should create new email and save it as a draft, verify that the mail is in the "Draft"', async () => { 
-        await mailPage.buttonMail.isDisplayed();
+        await expect (await mailPage.buttonMail.waitForDisplayed()).is.equal(true);
         await mailPage.buttonMail.click();
-        await mailPage.toField.isDisplayed();
-        await mailPage.toSubject.isDisplayed();
-        await mailPage.toBodyText.isDisplayed();
+        await expect (await mailPage.toField.waitForDisplayed()).is.equal(true);
+        await expect (await mailPage.toSubject.waitForDisplayed()).is.equal(true);
+        await expect (await mailPage.toBodyText.waitForDisplayed()).is.equal(true);
         await mailPage.inputEmail('sannav1985@gmail.com','Mail','Text'); 
         await draftMail.folderDraft.click();
         });
@@ -36,14 +34,14 @@ describe('My Login application', () => {
     it('should verify that the mail in the "Draft" contains same adress, subject, body, send it', async () => {
         
         await draftMail.mailInDraft.click(); 
-        expect (await draftMail.draftMailAdress.isDisplayed()).is.equal(true);
-        expect (await draftMail.draftMailSubject.isDisplayed()).is.equal(true);
-        expect (await draftMail.draftMailText.isDisplayed()).is.equal(true);
+        await expect (await draftMail.draftMailAdress.waitForDisplayed()).is.equal(true);
+        await expect (await draftMail.draftMailSubject.waitForDisplayed()).is.equal(true);
+        await expect (await draftMail.draftMailText.waitForDisplayed()).is.equal(true);
         await draftMail.buttonSent.click();        
            });
 
     it('should verify that email is disappeared from the draft folder', async () => {  
-        await draftMail.folderDraft.isDisplayed();
+        await expect (await draftMail.folderDraft.waitForDisplayed()).is.equal(true);
         await draftMail.folderDraft.click();
         await expect (await draftMail.draftMailAdress.isDisplayed()).is.equal(false);
         await expect (await draftMail.draftMailSubject.isDisplayed()).is.equal(false);
@@ -51,15 +49,15 @@ describe('My Login application', () => {
         });
 
     it('should verify that email is in the sent folder', async () => {
-        await sentFolderButton.sentPage.waitForDisplayed();
+        await expect (await sentFolderButton.sentPage.waitForDisplayed()).is.equal(true);
         await sentFolderButton.sentPage.click();
         await expect (await sentFolderButton.mailInSentFolder.waitForDisplayed()).is.equal(true);
     });
 
     it('should log off', async () => {
-        await textPassword.accountName.waitForDisplayed();
+        await expect (await textPassword.accountName.waitForDisplayed()).is.equal(true);
         await textPassword.accountName.moveTo();
-        await textPassword.logOffButton.waitForDisplayed()
+        await expect (await textPassword.logOffButton.waitForDisplayed()).is.equal(true);
         await textPassword.logOffButton.click();
     });
 }); 
